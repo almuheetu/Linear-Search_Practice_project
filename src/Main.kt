d2b2c3c091d4fd84924d1c02e07a046528016762
Almuheetu shihab <almuheetu@gmail.com>
2024-07-31 01:06:54 +0600

    Linear search fun and main fun implementation complete

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,26 @@
+fun linearSearch(array: IntArray, target: Int): Int {
+    for (index in array.indices) {
+        if (array[index] == target) {
+            return index
+        }
+    }
+    return -1
+}
+
 fun main() {
-    println("Hello World!")
+    val numbers = intArrayOf(10, 20, 30, 40, 50)
+    var target = 50
+    var result = linearSearch(numbers, target)
+    println(result)
+
+
+    target = 20
+    result = linearSearch(numbers, target)
+    println(result)
+
+    target = 76
+    result = linearSearch(numbers, target)
+    println(result)
+
+
 }
